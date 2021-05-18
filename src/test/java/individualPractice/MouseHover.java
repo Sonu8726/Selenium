ab372263f2352e16290f56b6bb88ea3f8a5c0b91
Sonu Kumar <sonukumar4100.sk@gmail.com>
2021-05-18 17:35:49 +0530

    Mouse hover validation

---
+++ src/test/java/individualPractice/MouseHover.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package individualPractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+/**
+ * @author Ghost
+ *
+ */
+public class MouseHover {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\lib\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		Actions actn = new Actions(driver);
+		WebElement mhover = driver.findElement(By.id("mousehover"));
+		actn.moveToElement(mhover).build().perform();
+
+		boolean topDisplay = driver.findElement(By.linkText("Top")).isDisplayed();
+		if (topDisplay == true) {
+			System.out.println("Print Work exact");
+		} else {
+			System.out.println("Something Failed");
+		}
+		Thread.sleep(8000);
+
+		driver.close();
+	}
+
+}
